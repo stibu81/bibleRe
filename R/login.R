e1380dc666f48bcadfcd47d7a5349b858e6c20ff
stibu81 <slanz1137@gmail.com>
2020-03-14 11:45:53 +0100

    add login functionality

---
+++ R/login.R
@@ -0,0 +1,86 @@
+#' Login to the Webinterface
+#'
+#' Login to the webinterface with username and password.
+#'
+#' @param username character giving the username, which usually
+#'  consists of 6 digits.
+#' @param password character giving the password. See also
+#'  the security warning under 'Details'.
+#'
+#' @details
+#' **Security warning:**
+#' It is possible that passwords are stored in clear text and
+#' can even be seen by employees of the library. This has
+#' been the case at some point and it is not known whether
+#' this has changed in the meantime. Your email address is stored
+#' as well. Do not use a password that is also used anywhere else!
+#'
+#' @return
+#' a `session` object
+#'
+#' @export
+
+bib_login <- function(username, password) {
+
+  url <- "http://katalog.iz-region-bern.ch/WebOPAC-Koeniz/account/login?returnUrl=%2FWebOPAC-Koeniz%2Faccount%2F"
+
+  session <- rvest::html_session(url)
+  form <- rvest::html_form(session)[[2]]
+  filled_form <- rvest::set_values(form,
+                                   Username = username,
+                                   Password = password)
+  rvest::submit_form(session,
+                     filled_form,
+                     "<unnamed>")
+
+}
+
+
+#' Read File With Login Data
+#'
+#' Read a JSON file with login data for one or more
+#' users.
+#'
+#' @param file character giving the name of a file.
+#'
+#' @details
+#' The JSON-file must have the following format:
+#'
+#' ```json
+#' {
+#'   "User1": {
+#'     "username": "123456",
+#'     "password": "password1"
+#'   },
+#'   "User2": {
+#'     "username": "104392",
+#'     "password": "password2"
+#'   },
+#'   "User3": {
+#'     "username": "101010",
+#'     "password": "password3"
+#'   }
+#' }
+#' ```
+#' A block must be included for each user and must contain
+#' the fields `username` and `password`. The above example is
+#' also contained as a file in the package. You can obtain
+#' the path to the file with
+#'
+#' ```r
+#' system.file("example/biblere_passwords.json",
+#'             package = "bibleRe")
+#' ```
+#'
+#' @export
+
+bib_read_login_data <- function(file) {
+
+  if (!file.exists(file)) {
+    warning("File", file, "does not exist.")
+    return(list())
+  }
+
+  jsonlite::fromJSON(file)
+
+}
