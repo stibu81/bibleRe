a1ac68689cdef2c9903a3f9328c1a861b8cab9d6
stibu81 <slanz1137@gmail.com>
2024-11-16 13:19:49 +0100

    get icon directly from library site

--- R/icon.R
+++ R/icon.R
@@ -3,15 +3,9 @@
 #' @param file path where the icon will be stored. If the name does
 #'  not end with ".ico", the ending is appended.
 #'
-#' @details
-#' This requires the package `faviconPlease` to be installed. Install it
-#' with `install.packages('faviconPlease')`.
-#'
 #' @export
 
 bib_get_icon <- function(file = "~/biblere.ico") {
-
-  rlang::check_installed("faviconPlease")
 
   file <- normalizePath(file, mustWork = FALSE)
   if (file.exists(file)) {
@@ -21,8 +15,22 @@ bib_get_icon <- function(file = "~/biblere.ico") {
     file <- paste0(file, ".ico")
   }
 
-  favicon <- faviconPlease::faviconDuckDuckGo("koenizerbibliotheken.ch")
+  favicon <- get_favicon_url()
   utils::download.file(favicon, file)
 
   invisible(file.exists(file))
+}
+
+
+# Helper function to determine the url of the favicon from the library's
+# webpage.
+
+get_favicon_url <- function() {
+
+  base_url <- "https://koenizerbibliotheken.ch/de"
+  rvest::read_html(base_url) %>%
+    rvest::html_elements("link[rel='icon']") %>%
+    rvest::html_attr("href") %>%
+    paste(base_url, ., sep = "/")
+
 }
