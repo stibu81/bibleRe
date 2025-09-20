f5ce380f173d10daab4689f63d43436f0bf6748c
stibu81 <slanz1137@gmail.com>
2025-09-20 13:51:33 +0200

    set up new dependencies

--- R/bibleRe-package.R
+++ R/bibleRe-package.R
@@ -36,3 +36,10 @@ bib_urls <- dplyr::tibble(
 )
 
 globalVariables(".")
+
+# dependencies chromote and R6 are never used in the code => use them here
+# to prevent complaints from R CMD check
+imports_dummy <- function() {
+  chromote::chrome_versions_list
+  R6::R6Class
+}
